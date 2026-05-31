ee4b303adea25789d1d8d1029a5c23f90e8e45b1
Ericknvp <narvaezvegaerick@gmail.com>
2026-05-31 17:20:07 -0500

    Utils: agregar helpers de redireccion web y stub para mobile

---
+++ lib/utils/web_redirect.dart
@@ -0,0 +1,10 @@
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html' as html;
+
+void redirectToLanding() {
+  html.window.location.replace('/landing.html');
+}
+
+String? getViewParam() {
+  return Uri.parse(html.window.location.href).queryParameters['view'];
+}
---
+++ lib/utils/web_redirect_stub.dart
@@ -0,0 +1,3 @@
+void redirectToLanding() {}
+
+String? getViewParam() => null;
