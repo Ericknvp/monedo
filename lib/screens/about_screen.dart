1939b9cfae732e8dd2b38691778db0954d6b8eb9
Ericknvp <narvaezvegaerick@gmail.com>
2026-05-31 16:51:36 -0500

    Acerca de: usar SVG del logo

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../theme/app_theme.dart';
@@ -32,11 +33,10 @@ class AboutScreen extends StatelessWidget {
                     color: AppTheme.primaryContainer,
                     borderRadius: BorderRadius.circular(20),
                   ),
-                  child: Image.asset(
-                    'assets/newmonedodesign/newlogomonedo.png',
+                  child: SvgPicture.asset(
+                    'assets/newmonedodesign/newlogomonedo.svg',
                     width: 64,
                     height: 64,
-                    fit: BoxFit.contain,
                   ),
                 ),
               ),
