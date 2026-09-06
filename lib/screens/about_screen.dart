22d20f63c73d70baaab993964708f3fd96967f20
Erick Narváez <narvaezvegaerick@gmail.com>
2026-09-06 14:38:55 -0500

    actualización de landing

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -49,7 +49,7 @@ class AboutScreen extends StatelessWidget {
                 ),
               ),
               Text(
-                'v2.2.0',
+                'v2.2.1',
                 style: GoogleFonts.beVietnamPro(
                     color: AppTheme.onSurfaceVariant, fontSize: 14),
               ),
